8b9c6c729b48fc10da92eb525bc8cb5024c80870
Fvtvl <fvtvlnight@gmail.com>
2023-02-26 18:08:39 +0000

    create rotate

--- src/components/Tetris/index.js
+++ src/components/Tetris/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { createStage } from '../../gameHelpers';
+import { checkCollision, createStage } from '../../gameHelpers';
 import { usePlayer } from '../../hooks/usePlayer';
 import { useStage } from '../../hooks/useStage';
 import Display from '../Display';
@@ -11,13 +11,15 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
-  const [stage, setStage] = useStage(player);
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log('re-render');
 
   const movePlayer = (dir) => {
-    updatePlayerPos({ x: dir, y: 0 });
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
+    }
   };
 
   const startGame = () => {
@@ -25,10 +27,21 @@ const Tetris = () => {
     // Reset everything
     setStage(createStage());
     resetPlayer();
+    setGameOver(false);
   };
 
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false });
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      // Game Over
+      if (player.pos.y < 1) {
+        console.log('GAME OVER!!!');
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
   };
 
   const dropPlayer = () => {
@@ -37,12 +50,14 @@ const Tetris = () => {
 
   const move = ({ keyCode }) => {
     if (!gameOver) {
-      if (keyCode === 37) {
+      if (keyCode === 37 || keyCode === 65) {
         movePlayer(-1);
-      } else if (keyCode === 39) {
+      } else if (keyCode === 39 || keyCode === 68) {
         movePlayer(1);
-      } else if (keyCode === 40) {
+      } else if (keyCode === 40 || keyCode === 83) {
         dropPlayer();
+      } else if (keyCode === 38 || keyCode === 87) {
+        playerRotate(stage, 1);
       }
     }
   };
