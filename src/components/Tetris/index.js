46d7bffaadc52eab0846918d457a81852db17f5a
Fvtvl <fvtvlnight@gmail.com>
2023-02-27 13:09:37 +0000

    create Interval, level,score, rows hooks

--- src/components/Tetris/index.js
+++ src/components/Tetris/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { checkCollision, createStage } from '../../gameHelpers';
+import { useGameStatus } from '../../hooks/useGameStatus';
+import { useInterval } from '../../hooks/useInterval';
 import { usePlayer } from '../../hooks/usePlayer';
 import { useStage } from '../../hooks/useStage';
 import Display from '../Display';
@@ -12,7 +14,9 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] =
+    useGameStatus(rowsCleared);
 
   console.log('re-render');
 
@@ -26,11 +30,19 @@ const Tetris = () => {
     console.log('test');
     // Reset everything
     setStage(createStage());
+    setDropTime(1000);
     resetPlayer();
     setGameOver(false);
+    setScore(0);
+    setLevel(0);
+    setRows(0);
   };
 
   const drop = () => {
+    if (rows > (level + 1) * 10) {
+      setLevel((prev) => prev + 1);
+      setDropTime(1000 / (level + 1) + 200);
+    }
     if (!checkCollision(player, stage, { x: 0, y: 1 })) {
       updatePlayerPos({ x: 0, y: 1, collided: false });
     } else {
@@ -44,7 +56,16 @@ const Tetris = () => {
     }
   };
 
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 40 || keyCode === 83) {
+        setDropTime(1000 / (level + 1) + 200);
+      }
+    }
+  };
+
   const dropPlayer = () => {
+    setDropTime(null);
     drop();
   };
 
@@ -62,8 +83,17 @@ const Tetris = () => {
     }
   };
 
+  useInterval(() => {
+    drop();
+  }, dropTime);
+
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
+    <StyledTetrisWrapper
+      role="button"
+      tabIndex="0"
+      onKeyDown={(e) => move(e)}
+      onKeyUp={keyUp}
+    >
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -71,9 +101,9 @@ const Tetris = () => {
             <Display gameOver={gameOver} text="Game Over" />
           ) : (
             <div>
-              <Display text="Score" />
-              <Display text="Rows" />
-              <Display text="Level" />
+              <Display text={`Score: ${score}`} />
+              <Display text={`Rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
             </div>
           )}
           <StartButton callback={startGame} />
---
+++ src/hooks/useGameStatus.js
@@ -0,0 +1,23 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export const useGameStatus = (rowsCleared) => {
+  const [score, setScore] = useState(0);
+  const [rows, setRows] = useState(0);
+  const [level, setLevel] = useState(0);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const linePoints = [40, 100, 300, 1200];
+
+  const calcStore = useCallback(() => {
+    if (rowsCleared > 0) {
+      setScore((prev) => prev + linePoints[rowsCleared - 1] * (level + 1));
+      setRows((prev) => prev + rowsCleared);
+    }
+  }, [level, linePoints, rowsCleared]);
+
+  useEffect(() => {
+    calcStore();
+  }, [calcStore, rowsCleared, score]);
+
+  return [score, setScore, rows, setRows, level, setLevel];
+};
